1a467a7f8255eaf517868c7d1187e0f7ce6b990f
ugpec79 <ujjawalguptapec@gmail.com>
2024-10-17 00:32:22 +0530

    FRUI

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,16 +2,7 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Uploader } from "@/components/Uploader";
 
 export default function Home() {
   return (
@@ -20,6 +11,7 @@ export default function Home() {
         <Navbar />
         <section className="bg-gray-900 text-white text-center py-20">
           <Hero />
+          <Uploader/>
         </section>
       </div>
     </>
