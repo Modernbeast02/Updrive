ca9f2bd05c0a2a576159fd0bcae7576f882a3292
Yashita Bansal <yashitabansal39@gmail.com>
2024-10-17 04:01:39 +0530

    #Flash Backend

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,37 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
 import os
+from flask import Flask, jsonify, request  # Import request from Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 UPLOAD_FOLDER = 'static/UploadedPdfs'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Ensure the upload folder exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    print("hi")
+    # Use the request object from Flask to access the uploaded file
     if 'file' not in request.files:
-        return 'No file part', 400
+        return jsonify({'error': 'No file part in the request'}), 400
 
     file = request.files['file']
 
     if file.filename == '':
-        return 'No selected file', 400
+        return jsonify({'error': 'No file selected for uploading'}), 400
 
-    # Save the file to the uploads directory
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-
-    return 'File uploaded successfully', 200
-
+    if file:
+        # Save the file to the upload folder
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        return jsonify({'message': 'File uploaded successfully', 'file_path': file_path}), 200
+    else:
+        return jsonify({'error': 'Failed to upload file'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
