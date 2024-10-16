56ed4152e074f9a7c0bb0e60816c86eefe4e4ca3
Yashita Bansal <yashitabansal39@gmail.com>
2024-10-16 22:55:12 +0530

    #Landing Page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <section className=" text-[#b7e4ea] text-center py-20">
+      <h1 className="p-5 text-7xl mb-6 font-generalsans">Welcome To UPDRIVE</h1>
+      <p className="text-6xl mb-6 font-generalsans text-color-gradient2">
+        Smart PDF Companion
+      </p>
+      <p className="text-lg mb-6 font-generalsans text-color-gradient2">
+        Your one stop destination to upload pdfs
+      </p>
+      <div className="flex justify-center items-center">
+        <button className=" text-white py-2 px-6 rounded-lg getStartedButton">
+          Get Started
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        generalsans: ["Generalsans Variable", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
