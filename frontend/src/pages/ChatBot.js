963995818e79c28fd06764478e53762f3c5a9160
Ankur Gupta <ankurgupta.bt21cse@pec.edu.in>
2024-10-17 09:00:00 +0530

    #Buttons

--- frontend/src/pages/ChatBot.js
+++ frontend/src/pages/ChatBot.js
@@ -2,21 +2,48 @@ import React, { useState, useRef, useEffect } from "react";
 import { FaUser, FaPaperPlane } from "react-icons/fa";
 import { useRouter } from "next/router";
 const ChatBot = (props) => {
-  const [processedString, setProcessedString] = useState('');
+  const [processedString, setProcessedString] = useState("");
   const initialMessages = [
     {
       id: 1,
       user: "Bot",
       text: "Hey! Ask me a question.",
-      timestamp:  new Date().toLocaleTimeString([], {
+      timestamp: new Date().toLocaleTimeString([], {
         hour: "2-digit",
         minute: "2-digit",
       }),
     },
   ];
+  const startingButtons = [
+    {
+      id: 1,
+      text: "Ask me about PDFs",
+      user: "Bot",
+    },
+    {
+      id: 2,
+      text: "What is a Smart PDF Companion?",
+      user: "Bot",
+    },
+    {
+      id: 3,
+      text: "How to upload a PDF? just writing this random msg to increase length hehe",
+      user: "Bot",
+    },
+    {
+      id: 4,
+      text: "PDF features",
+      user: "Bot",
+    },
+    {
+      id: 5,
+      text: "Need help with a question",
+      user: "Bot",
+    },
+  ];
 
   const router = useRouter();
-  const {query} = router;
+  const { query } = router;
   const param1 = query.name;
   const [messages, setMessages] = useState(initialMessages);
   const [inputMessage, setInputMessage] = useState("");
@@ -24,73 +51,70 @@ const ChatBot = (props) => {
   const [isTyping, setIsTyping] = useState(false);
   const chatEndRef = useRef(null);
 
-    const fetchProcessedString = async () => {
-      try {
-        const response = await fetch(`http://127.0.0.1:5000/query?input_string=${inputMessage}`);
-        const data = await response.json();
-        setProcessedString(data.result);
+  // const fetchProcessedString = async () => {
+  //   try {
+  //     const response = await fetch(`http://127.0.0.1:5000/query?input_string=${inputMessage}`);
+  //     const data = await response.json();
+  //     setProcessedString(data.result);
 
-        // Add the processed string as a new message from the Bot
-        const botResponseMessage = {
-          id: messages.length + 1,
-          user: "Bot",
-          text: data.result,
-          timestamp: new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          }),
-          citations:['Yes', 'No']
-        };
-        setMessages((prevMessages) => [...prevMessages, botResponseMessage]);
-      } catch (error) {
-        console.error('Error fetching processed string:', error);
-      }
-    };
-    const fetchCitations = async () => {
-      // try {
-      //   const response = await fetch(`http://127.0.0.1:5000/citation`);
-      // } catch (error) {
-      //   console.error('Error fetching processed string:', error);
-      // }
-        try {
-          const response = await fetch('http://127.0.0.1:5000/citation');
+  //     // Add the processed string as a new message from the Bot
+  //     const botResponseMessage = {
+  //       id: messages.length + 1,
+  //       user: "Bot",
+  //       text: data.result,
+  //       timestamp: new Date().toLocaleTimeString([], {
+  //         hour: "2-digit",
+  //         minute: "2-digit",
+  //       }),
+  //       citations:['Yes', 'No']
+  //     };
+  //     setMessages((prevMessages) => [...prevMessages, botResponseMessage]);
+  //   } catch (error) {
+  //     console.error('Error fetching processed string:', error);
+  //   }
+  // };
+  // const fetchCitations = async () => {
+  //   // try {
+  //   //   const response = await fetch(`http://127.0.0.1:5000/citation`);
+  //   // } catch (error) {
+  //   //   console.error('Error fetching processed string:', error);
+  //   // }
+  //     try {
+  //       const response = await fetch('http://127.0.0.1:5000/citation');
 
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
+  //       if (!response.ok) {
+  //           throw new Error('Network response was not ok');
+  //       }
 
-          // Get the response as a Blob
-          const blob = await response.blob();
+  //       // Get the response as a Blob
+  //       const blob = await response.blob();
 
-          // Create a link element
-          const link = document.createElement('a');
-          link.href = URL.createObjectURL(blob);
-          link.download = 'citation.pdf'; // specify the desired file name
+  //       // Create a link element
+  //       const link = document.createElement('a');
+  //       link.href = URL.createObjectURL(blob);
+  //       link.download = 'citation.pdf'; // specify the desired file name
 
-          // Append to the body (needed for Firefox)
-          document.body.appendChild(link);
+  //       // Append to the body (needed for Firefox)
+  //       document.body.appendChild(link);
 
-          // Trigger the download
-          link.click();
+  //       // Trigger the download
+  //       link.click();
 
-          // Clean up and remove the link
-          document.body.removeChild(link);
-          URL.revokeObjectURL(link.href); // free up memory
-      } catch (error) {
-          console.error('There was a problem with the fetch operation:', error);
-      }
-    };
+  //       // Clean up and remove the link
+  //       document.body.removeChild(link);
+  //       URL.revokeObjectURL(link.href); // free up memory
+  //   } catch (error) {
+  //       console.error('There was a problem with the fetch operation:', error);
+  //   }
+  // };
 
+  // useEffect(() => {
+  //   scrollToBottom();
+  // }, [messages]);
 
- 
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-
-  const scrollToBottom = () => {
-    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
+  // const scrollToBottom = () => {
+  //   chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  // };
 
   const handleInputChange = (e) => {
     setInputMessage(e.target.value);
@@ -116,8 +140,7 @@ const ChatBot = (props) => {
     setMessages([...messages, newMessage]);
     setInputMessage("");
     setError("");
-    await fetchProcessedString()
-
+    await fetchProcessedString();
   };
 
   const handleKeyPress = (e) => {
@@ -126,23 +149,24 @@ const ChatBot = (props) => {
     }
   };
 
-  const handleOptionClick = (id, option) => {
-    if (option === "Reply") {
-      const messageToReply = messages.find((msg) => msg.id === id);
-      setInputMessage(`@${messageToReply.user} `);
-    } else if (option === "Delete") {
-      setMessages(messages.filter((msg) => msg.id !== id));
-    }
-  };
+  // const handleOptionClick = (id, option) => {
+  //   if (option === "Reply") {
+  //     const messageToReply = messages.find((msg) => msg.id === id);
+  //     setInputMessage(`@${messageToReply.user} `);
+  //   } else if (option === "Delete") {
+  //     setMessages(messages.filter((msg) => msg.id !== id));
+  //   }
+  // };
 
   return (
     <>
       <div className="flex justify-center items-center -mb-16 mt-12 text-lg">
         {param1}
       </div>
+
       <div className=" p-4 bg-[#0A1B2E] rounded-lg shadow-lg mt-20 mr-20 ml-20">
         <div className="bg-[#0A1B2E] rounded-lg shadow-inner p-4 min-h-[500px] overflow-y-auto">
-          {messages.map((message) => (
+          {initialMessages.map((message) => (
             <div key={message.id} className={`mb-4 animate-fade-in`}>
               <div
                 className={`flex ${
@@ -172,22 +196,40 @@ const ChatBot = (props) => {
                   </div>
                 </div>
               </div>
-              {message.citations&&( <div className="flex justify-start mt-2">
-              <p className="text-md text- white text-right mt-1 mr-2 opacity-75">
-                Do You want Citations?
-              </p>
-                <button className="bg-green-500 text-white rounded px-4 py-1 mr-2" onClick={fetchCitations}>
-                  Yes
-                </button>
-                <button className="bg-red-500 text-white rounded px-4 py-1">
-                  No
-                </button>
-              </div>)}
-              
+              {message.citations && (
+                <div className="flex justify-start mt-2">
+                  <p className="text-md text- white text-right mt-1 mr-2 opacity-75">
+                    Do You want Citations?
+                  </p>
+                  <button
+                    className="bg-green-500 text-white rounded px-4 py-1 mr-2"
+                    onClick={fetchCitations}
+                  >
+                    Yes
+                  </button>
+                  <button className="bg-red-500 text-white rounded px-4 py-1">
+                    No
+                  </button>
+                </div>
+              )}
             </div>
           ))}
+          <div className="flex-col  mb-4">
+            {startingButtons.map((button) => (
+              <div className="ml-10 mb-4 w-[450px]">
+                <button
+                  key={button.id}
+                  onClick={() => handleButtonClick(button.text)}
+                  className="bg-[#0A1B2E] w-[450px] text-[#5A82B8] border-[#5A82B8] border px-4 py-2 rounded-lg hover:scale-[1.05] duration-300"
+                >
+                  {button.text}
+                </button>
+              </div>
+            ))}
+          </div>
           <div ref={chatEndRef} />
         </div>
+
         <div className="mt-4">
           <div className="flex items-center">
             <input
