7dc794fd12e0e77beddd433683e3f67101fd8023
Ankur Gupta <ankurgupta.bt21cse@pec.edu.in>
2024-10-17 09:23:33 +0530

    # UI Changes

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import styles from '../styles/loader.module.css'
+import React from "react";
+import styles from "../styles/loader.module.css";
 
 const Loader = () => {
   return (
     <>
-    <div className={styles.body}>
-    <section className={styles.sectionStyle}>
-    <div className={styles.dot}></div>
-    </section>
-    </div>
+      <div className="-mt-56">
+        <div className={styles.body}>
+          <section className={styles.sectionStyle}>
+            <div className={styles.dot}></div>
+          </section>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
